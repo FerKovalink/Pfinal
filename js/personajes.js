82b042a7ace890d1aa2f12d4c1f1b7f8b42ca8e5
Kova <ferkovalink@gmail.com>
2022-09-27 04:32:43 -0300

    draco

--- js/personajes.js
+++ js/personajes.js
@@ -148,7 +148,7 @@ function verMinis() {
         } else if (bgg == 4) {
             bg.style.backgroundImage = "url('https://github.com/FerKovalink/d-d/blob/master/img/minis/humano.jpg?raw=true')"
         } else if (bgg == 5) {
-            bg.style.backgroundImage = "url('https://github.com/FerKovalink/d-d/blob/master/img/minis/dragon3.png?raw=true')"
+            bg.style.backgroundImage = "url('https://github.com/FerKovalink/d-d/blob/master/img/minis/dragon4.png?raw=true')"
         }
 
 
